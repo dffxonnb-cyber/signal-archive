5bc3bbb8d80cd8996d6a842d6cd1e626d03a5104
최다빈 <dffxonnb@gmail.com>
2026-04-25 20:13:48 +0900

    Remove writing from top navigation

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -7,7 +7,6 @@ const navigation = [
   { href: "/", label: "Home" },
   { href: "/projects", label: "Projects" },
   { href: "/case-studies", label: "Case Studies" },
-  { href: "/writing", label: "Writing" },
   { href: "/resume", label: "Resume" },
   { href: "/contact", label: "Contact" },
 ];
