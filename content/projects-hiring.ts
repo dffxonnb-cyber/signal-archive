fc63cbd2980c08a24147aeec251ed116cbb4530d
최다빈 <dffxonnb@gmail.com>
2026-07-16 10:07:10 +0900

    Route portfolio through DecisionOps current override

--- content/projects-hiring.ts
+++ content/projects-hiring.ts
@@ -3,7 +3,7 @@ import type { Project } from "../types/content";
 import {
   featuredProjects as sourceFeaturedProjects,
   projects as sourceProjects,
-} from "./projects-current";
+} from "./projects-decisionops";
 
 const PRIVATE_REPOSITORY_PROJECTS = new Set([
   "job-signal-pipeline",
